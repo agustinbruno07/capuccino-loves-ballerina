c3e93d0a6402ecdc0629801dba035272609debad
Agustin Bruno <brunoagustin562@gmail.com>
2026-08-04 20:16:20 -0300

    Creo pantallaMenu y lo conecte al main

---
+++ core/src/main/java/com/capuccinolovesballerina/game/pantallas/PantallaMenu.java
@@ -0,0 +1,52 @@
+package com.capuccinolovesballerina.game.pantallas;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class PantallaMenu implements Screen {
+    private Texture fondo;
+    private SpriteBatch batch;
+
+    @Override
+    public void show() {
+        fondo = new Texture("interfaz/fondo_menu.png");
+        batch = new SpriteBatch();
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        batch.begin();
+        batch.draw(fondo, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        fondo.dispose();
+        batch.dispose();
+    }
+}
