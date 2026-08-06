60faa7880ddcaaf206cb41ad9f34d83a9ed2fb1f
Agustin Bruno <brunoagustin562@gmail.com>
2026-08-05 23:12:30 -0300

    feat: Agrego PantallaOpciones

---
+++ core/src/main/java/com/capuccinolovesballerina/game/pantallas/PantallaOpciones.java
@@ -0,0 +1,42 @@
+package com.capuccinolovesballerina.game.pantallas;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class PantallaOpciones implements Screen {
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(0.1f, 0.5f, 0.2f, 1f);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
