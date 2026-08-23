57cac622402be98987bdd5681100269e09464cd5
Agustin <Brunoagustin562@gmail.com>
2026-08-23 13:41:15 -0300

    refactor: centralizar resolucion virtual y ruta del skin en la clase Constantes

---
+++ core/src/main/java/com/capuccinolovesballerina/game/Utilidades/Constantes.java
@@ -0,0 +1,8 @@
+package com.capuccinolovesballerina.game.Utilidades;
+
+public class Constantes {
+    public static final float ANCHO_MUNDO = 1366;
+    public static final float ALTO_MUNDO = 768;
+    public static final String RUTA_SKIN = "skin/uiskin.json";
+
+}
