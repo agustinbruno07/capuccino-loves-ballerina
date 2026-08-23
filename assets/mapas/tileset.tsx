a934683a09ecc81510656714f9d7a31e7efc5fcf
SANTIPRO <santinofierro08@gmail.com>
2026-08-23 20:48:23 -0300

    feat(arte): tileset, fondo y nivel0.tmx con colisiones por tile y capa de objetos

---
+++ assets/mapas/tileset.tsx
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="tileset" tilewidth="1408" tileheight="768" tilecount="5" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="4">
+  <image source="TILE ABAJO DEL PISO.png" width="64" height="64"/>
+  <objectgroup>
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <image source="TILE PARED DERECHA.png" width="64" height="64"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="15" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <image source="TILE PARED IZQUIERDA.png" width="64" height="64"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="49" y="0" width="15" height="64"/>
+   <object id="2" x="49" y="0" width="15" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <image source="TILE PISO.png" width="64" height="64"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+   <object id="2" x="0" y="0" width="64" height="64"/>
+   <object id="3" x="0" y="0" width="64" height="64"/>
+   <object id="4" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="18">
+  <image source="FONDO.png" width="1408" height="768"/>
+ </tile>
+</tileset>
